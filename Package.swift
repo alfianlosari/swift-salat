16e22f5c19892304bb50ccb326fbe726e33bdd2a
Alfian Losari <alfianlosari@gmail.com>
2018-05-23 20:45:11 +0700

    Initial Commit

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift_cmd",
+    name: "swift_salat",
     dependencies: [
          .package(url: "https://github.com/tid-kijyun/Kanna.git", from: "4.0.0")
         // Dependencies declare other packages that this package depends on.
@@ -14,7 +14,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "swift_cmd",
+            name: "swift_salat",
             dependencies: ["Kanna"]),
     ]
 )
--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# swift_cmd
+# swift_salat
 
-A description of this package.
+Hayya alas Salah - Inspired by aldidana go-salat (https://github.com/aldidana/go-salat)
+
+## Installation
+
+* git clone
+* swift build
+* .build/debug/swift_salat
+
+## Example
+
+![alt salat](https://storage.googleapis.com/alfian-162712.appspot.com/Screen%20Shot%202018-05-23%20at%2020.43.08.png)
